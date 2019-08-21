ccebbfcb71a3499ea5ef6149a96abef36f7a5fde
Changbai Guo <33477119+CBVeLuMe@users.noreply.github.com>
2019-08-21 11:44:12 -0400

    Initial the Testing Build

---
+++ HolierThanThou/Assets/Scripts/InspectorEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class InspectorEditor : MonoBehaviour
+{
+    [CustomEditor(typeof(Renamer))]
+    public class RenamerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            Renamer renamer = (Renamer)target;
+
+            if (GUILayout.Button("Rename Children"))
+            {
+                renamer.RenameChildren();
+            }
+        }
+    }
+}
---
+++ HolierThanThou/Assets/Shijun/Examples/JumpButtonExample.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JumpButtonExample : MonoBehaviour
+{
+
+    public float speed;
+
+    public Button jumpButton;
+
+    private void Update()
+    {
+        AltJumpControl();
+
+        if (canJump)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * speed, ForceMode.Impulse);
+            canJump = !canJump;
+        }
+    }
+
+    private bool canJump = false;
+
+    public void PlayerJump()
+    {
+        if (!canJump)
+        {
+            canJump = !canJump;
+        }
+    }
+
+    private void AltJumpControl()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //canJump = !canJump;
+            //jumpButton.onClick.Invoke();
+
+            jumpButton.Select();
+        }
+
+        
+
+    }
+
+}
