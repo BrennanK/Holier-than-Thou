c9001d672d58d81a0722e66f505d7c6761abb29f
Changbai Guo <33477119+CBVeLuMe@users.noreply.github.com>
2019-08-22 20:56:14 -0400

    Add a Camera Track and Adjust Driction Methods

---
+++ HolierThanThou/Assets/Shijun/ExampleJump/ExampleScripts/CameraTrackExample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrackExample : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = target.position - this.transform.position;
+    }
+
+    private void Update()
+    {
+        this.transform.position = target.position - offset;
+    }
+}
