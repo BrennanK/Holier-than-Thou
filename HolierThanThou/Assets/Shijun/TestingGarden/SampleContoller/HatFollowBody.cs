956260c5d9dc48a1eb99ddf3ec2ccf9e1d371f61
Changbai Guo <33477119+CBVeLuMe@users.noreply.github.com>
2019-09-19 15:57:52 -0400

    Adjust Movement Methods Slightly

---
+++ HolierThanThou/Assets/Shijun/TestingGarden/SampleContoller/HatFollowBody.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatFollowBody : MonoBehaviour
+{
+    private Transform hatTransform;
+    public Transform bodyTransform;
+
+    private void Start()
+    {
+        hatTransform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        hatTransform.position = new Vector3(bodyTransform.position.x, hatTransform.position.y, bodyTransform.position.z);
+        //hatTransform.position.x = bodyTransform.position.x;
+    }
+}
