9c3bb3e4ccb025655e110de0760d08aaaf9749ee
Shiaw-Rong Lin <lin_rong2005@hotmail.com>
2019-10-17 11:02:42 -0400

    Player and AI movement disabled at game start and goal hit
    Both player and AI are now restricted movement at the start of the game and when hitting the goal while waiting for respawn.
    New Terrain for AI test level is also added.

--- HolierThanThou/Assets/Scripts/GamePlay/SpawnPointManager.cs
+++ HolierThanThou/Assets/Scripts/GamePlay/SpawnPointManager.cs
@@ -7,7 +7,6 @@ public class SpawnPointManager : MonoBehaviour
     private GameObject[] spawnPoints;
 
 
-
     public List<Competitor> players = new List<Competitor>();
     List<GameObject> startPoints = new List<GameObject>();
 
@@ -26,6 +25,7 @@ public class SpawnPointManager : MonoBehaviour
         }
 
         SpawnPlayers();
+        
     }
 
     private void SpawnPlayers()
@@ -36,13 +36,13 @@ public class SpawnPointManager : MonoBehaviour
 
             player.gameObject.transform.position = startPoints[rand].transform.position;
             startPoints.Remove(startPoints[rand]);
-
+            StartCoroutine(PauseRigidBodyControl(player, 4f));
         }
+        
     }
 
     public void RespawnPlayer(string nameX)
     {
-
         var _competitior = players.Find(x => x.Name == nameX);
         _competitior.transform.position = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
         _competitior.ScoredGoal = false;
@@ -52,8 +52,27 @@ public class SpawnPointManager : MonoBehaviour
     {
         yield return new WaitForSeconds(2f);
         RespawnPlayer(name);
-
     }
 
-
+    public IEnumerator PauseRigidBodyControl(Competitor competitor, float duration)
+    {
+        if (competitor.GetComponent<RigidBodyControl>())
+        {
+            competitor.GetComponent<RigidBodyControl>().enabled = false;
+        }
+        else
+        {
+            competitor.GetComponent<AIBehavior>().enabled = false;
+        }
+        yield return new WaitForSeconds(duration);
+        if (competitor.GetComponent<RigidBodyControl>())
+        {
+            competitor.GetComponent<RigidBodyControl>().enabled = true;
+            
+        }
+        else
+        {
+            competitor.GetComponent<AIBehavior>().enabled = true;
+        }
+    }
 }
