9c822a7959edca9dccaa85c0fc51031a8ade1686
Dylan Bronstein <dcbronstein@student.fullsail.edu>
2019-10-18 10:13:30 -0400

    Revert "Revert "Garden_New""
    This reverts commit d848b5bf33e2356f8f68c4418d54360f38394fdb.

--- HolierThanThou/Assets/Scripts/GamePlay/Goal.cs
+++ HolierThanThou/Assets/Scripts/GamePlay/Goal.cs
@@ -10,24 +10,23 @@ public class Goal : MonoBehaviour
     [SerializeField]
     private PowerUpEditor powerUpEditor;
     private PowerUp powerUp;
+    private PointTracker pointTracker;
+    
 
     public float radius;
     public float disToGround;
     public float power;
     public float upwardForce;
     public float playerPower;
-    public float playerUpwardForce;
     public LayerMask ground;
-    Vector3 explodePosition;
 
-    private int point = 1;
+    private int point;
 
     private void Start()
     {
         spawnPointManager = gameObject.GetComponent<SpawnPointManager>();
         scoreManager = gameObject.GetComponent<ScoreManager>();
-        //powerUp = new BlastZone(false, powerUpEditor.BZ_hasDuration, powerUpEditor.BZ_duration, radius, power, upwardForce);
-        explodePosition = new Vector3(transform.position.x, transform.position.y + 3.5f, transform.position.z);
+        powerUp = new BlastZone(false, powerUpEditor.BZ_hasDuration, powerUpEditor.BZ_duration, radius, power, upwardForce);
     }
 
 
@@ -35,14 +34,18 @@ public class Goal : MonoBehaviour
     {
         var _competitor = other.gameObject.GetComponent<Competitor>();
 
-        if (_competitor)
+        if(_competitor)
         {
+            pointTracker = _competitor.GetComponentInParent<PointTracker>();
+            point = pointTracker.PointVal();
             _competitor.ScoredGoal = true;
+            pointTracker.ResetMult();
+            pointTracker.ResetBasePoints();
             scoreManager.UpdateScore(_competitor.Name, point);
             StartCoroutine(spawnPointManager.RespawnTimer(_competitor.Name));
-            StartCoroutine(spawnPointManager.PauseRigidBodyControl(_competitor, 2f));
             Explosion();
         }
+
     }
 
     void Explosion()
@@ -69,28 +72,26 @@ public class Goal : MonoBehaviour
                 var rb = enemy.GetComponent<Rigidbody>();
                 var competitor = enemy.GetComponent<Competitor>();
 
-                if (enemy.tag == "Player")
+
+                if(enemy.tag == "Player")
                 {
-                    rb.AddExplosionForce(playerPower, explodePosition, radius, playerUpwardForce, ForceMode.Impulse);
+                    rb.AddExplosionForce(playerPower, transform.position, radius, upwardForce, ForceMode.Impulse);
                 }
                 else
                 {
                     competitor.navMeshOff = true;
-                    rb.AddExplosionForce(power, explodePosition, radius, upwardForce, ForceMode.Impulse);
+                    rb.AddExplosionForce(power, transform.position, radius, upwardForce, ForceMode.Impulse);
 
                     if (Physics.Raycast(competitor.transform.position, Vector3.down, disToGround, ground) == true)
                     {
                         competitor.BeenBlasted();
                     }
                 }
+
+
+                    
+                
             }
         }
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        // Draw a semitransparent blue cube at the transforms position
-        Gizmos.color = new Color(1, 0, 0, 0.5f);
-        Gizmos.DrawCube(explodePosition, new Vector3(1, 1, 1));
     }
 }
