720bb995a956cd8de76f43eae7d9f521d7d8b5ad
Guilherme Oliveira <guilherme.061@gmail.com>
2019-10-18 11:51:53 -0400

    reverting merge from big biffy branch

--- HolierThanThou/Assets/Scripts/GamePlay/ScoreManager.cs
+++ HolierThanThou/Assets/Scripts/GamePlay/ScoreManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    public List<Competitor> players;
+    public List<Competitor> players = new List<Competitor>();
 
     public Text scoreText;
     public Text endGameScore;
