a91993085172b8f83e03057e16fee66f2abe1f1d
Dylan Bronstein <dcbronstein@student.fullsail.edu>
2019-10-23 16:50:13 -0400

    pushing placeholder UI

---
+++ HolierThanThou/Assets/Scripts/GamePlay/PlayerUIScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUIScript : MonoBehaviour
+{
+    GameObject player;
+    public Text multiplier;
+    public Text speed;
+    private float mag;
+    private Vector3 vel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vel = player.GetComponent<Rigidbody>().velocity;
+        mag = vel.magnitude;
+        multiplier.text = player.GetComponent<PointTracker>().MultVal().ToString();
+        speed.text = mag.ToString();
+    }
+}
