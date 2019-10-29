0e9802a37b28b529ae7cb09d09e2f3234df1125e
TylerBaker36 <51458700+TylerBaker36@users.noreply.github.com>
2019-10-29 13:24:28 -0400

    Achievements
    Achievements have been added except for the ui stuff

---
+++ HolierThanThou/Assets/Scripts/StoreServices/Core/AchievementUIManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AchievementUIManager : MonoBehaviour
+{
+    [Header("UI Integration")]
+    public GameObject prefabEntry;
+    public GameObject prefabCanvas;
+    public AchievementUI[] achievementsInUI;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StoreServices.Core.Achievements.AchievementInstance[] achievementInstances = StoreServices.AchievementManager.instance.AchievementInstances;
+
+        if(achievementInstances.Length == achievementsInUI.Length)
+        {
+            for(int i = 0; i < achievementsInUI.Length; i++)
+            {
+                float percentageProgress = achievementInstances[i].ProgressInPercentage;
+                percentageProgress *= 100;
+                achievementsInUI[i].UpdateAchievement(achievementInstances[i].AchievementName, achievementInstances[i].AchievementDescription, $"{Mathf.Round(percentageProgress)}%");
+            }
+        }
+        else
+        {
+            Debug.Log($"Achievements on game and achievements on UI are not the same!");
+        }
+        prefabCanvas.SetActive(false);
+    }
+
+    public void ShowAchievements()
+    {
+        prefabCanvas.SetActive(true);
+    }
+
+    public void HideAchievement()
+    {
+        prefabCanvas.SetActive(false);
+    }
+}
