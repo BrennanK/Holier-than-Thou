c6556ec6bcd0511714b9b9284acf04d4ea758a4f
Dylan Bronstein <dcbronstein@student.fullsail.edu>
2019-11-07 17:27:58 -0500

    new garden push (for real dominique)

---
+++ HolierThanThou/Assets/Scripts/GamePlay/Garden_New Specific/AnyDirBoost.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnyDirBoost : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float boostVal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        rb = other.GetComponentInParent<Rigidbody>();
+        rb.AddForce(rb.velocity * boostVal);
+    }
+}
